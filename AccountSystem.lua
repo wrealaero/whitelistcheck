13dd10c046136b12ff9914a4df9f3e245f110cc5
wrealaero <clanwarobi@gmail.com>
2025-12-23 22:43:24 -0600

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -4,7 +4,7 @@ return {
             Username = "aero",
             Password = "aero123",
             IsActive = true,
-            HWID = "", 
+            HWID = "204d0b5ae4fb36b5b6992b27b12d66f1ff70cfb65f4d53", 
             AllowedHWIDs = {}
         },
     }
