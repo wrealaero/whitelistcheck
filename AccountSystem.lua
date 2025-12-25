854faa05856c84ff365a046eb3e2c938e4014ffb
wrealaero <clanwarobi@gmail.com>
2025-12-24 20:43:50 -0600

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -7,5 +7,131 @@ return {
             HWID = "204d0b5ae4fb36b5b6992b27b12d66f1ff70cfb65f4d53", 
             AllowedHWIDs = {}
         },
+        {
+            Username = "min",
+            Password = "min123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "theyfearsavy",
+            Password = "theyfearsavy123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "mosa",
+            Password = "mosa1234",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "ronjamaican",
+            Password = "ronjamaican123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "sho",
+            Password = "sho123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "synv4",
+            Password = "synv4123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "dexter",
+            Password = "dexter123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "pvpmentality",
+            Password = "pvpmentality123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "generalcyan",
+            Password = "generalcyan123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "wayys",
+            Password = "wayys123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "shame",
+            Password = "shame123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "gwy",
+            Password = "gwy123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "abstract",
+            Password = "abstract123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "desire",
+            Password = "desire123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "breezy",
+            Password = "breezy123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "kiwi",
+            Password = "kiwi123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "gav",
+            Password = "gav123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        },
+        {
+            Username = "croc",
+            Password = "croc123",
+            IsActive = true,
+            HWID = "",
+            AllowedHWIDs = {}
+        }
     }
 }
